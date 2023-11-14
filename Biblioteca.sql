c9eb8ab2197390ae63a31127dd62de2ff0941347
gabrielviveros <97645126+gabrielviveros@users.noreply.github.com>
2023-11-14 20:16:14 -0300

    Update Biblioteca.sql

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -3,20 +3,19 @@ CREATE DATABASE Biblioteca;
 USE Biblioteca;
 
 CREATE TABLE LIBROS (
-        ID_TITULO INT AUTO_INCREMENT primary key,
+        ID_Titulo INT AUTO_INCREMENT primary key,
         Autor VARCHAR(60) NOT NULL,
         Titulo VARCHAR(100) NOT NULL,
         Año_Edición INT(20),
         Editorial VARCHAR(60),
         Materia VARCHAR(60)
-    )
-    '''
+    );
 
 CREATE TABLE USUARIOS (
-        ID_NOMBRES INT AUTO_INCREMENT primary key,
-        ID DNI int (12) ,
+        ID_DNI INT AUTO_INCREMENT primary key,
+        DNI INT (20) NOT NULL,
         Nombres VARCHAR(50) NOT NULL,
         Apellidos VARCHAR(50) NOT NULL,
-        Telefono INT(20),https://paiza.io/
-        Direccion VARCHAR(45),
-    )
+        Telefono INT(20) NOT NULL,
+        Direccion VARCHAR(45) NOT NULL
+    );
